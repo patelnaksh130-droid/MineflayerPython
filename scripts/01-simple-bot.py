67e358bb4a983d51ac90c12b57a2f569b0d824b6
patelnaksh130-droid <patelnaksh130@gmail.com>
2025-08-06 17:45:28 +0530

    Update 01-simple-bot.py

--- scripts/01-simple-bot.py
+++ scripts/01-simple-bot.py
@@ -5,7 +5,7 @@ mineflayer = require("mineflayer")
 
 # Create bot with basic parameters
 bot = mineflayer.createBot(
-    {"username": "simple-bot", "host": "localhost", "port": 3000, "version": "1.19.4", "hideErrors": False}
+    {"username": "simple-bot", "host": "beetlepro0024-FHWn.aternos.me:20026", "port": 20026, "version": "1.21.8", "hideErrors": False}
 )
 
 # Login event required for bot
